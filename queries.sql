a4a13c8b31732ffd8546bbb54ed8052fc8baa097
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-03-16 13:23:06 +0000

    finished all query practise

--- queries.sql
+++ queries.sql
@@ -10,7 +10,7 @@ ON Product.CategoryId = Category.Id
 
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
-SELECT "Shipper".CompanyName, "Order".Id 
+SELECT "Order".Id, "Shipper".CompanyName
 FROM "Order" 
 JOIN "Shipper"
 ON "Order".ShipVia = "Shipper".Id
@@ -18,10 +18,21 @@ WHERE "Order".OrderDate < '2012-08-09'
 
 
 
+
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
-
+SELECT "Product".ProductName, "OrderDetail".Quantity
+FROM "Product"
+JOIN "OrderDetail"
+ON "Product".Id = "OrderDetail".ProductId
+WHERE "OrderDetail".OrderId = 10251
 
 
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+-- Order , Customer, Employee tables 
+
+SELECT "Order".Id, "Customer".CompanyName AS CustomerCompany, "Employee".LastName AS EmployeeLastName
+FROM "Order" 
+JOIN "Employee" ON "Employee".Id = "Order".EmployeeId
+JOIN "Customer" ON "Order".CustomerId = "Customer".Id
